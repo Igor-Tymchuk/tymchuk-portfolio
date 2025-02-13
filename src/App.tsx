389dc038cad4173eb0df7b31cbdc26c2d3a4b282
Igor Tymchuk <dinamit.shuher@gmail.com>
2025-02-13 14:06:58 +0200

    attempt

--- src/App.tsx
+++ src/App.tsx
@@ -3,83 +3,83 @@ import Background from "./assets/Background.ts";
 import LogoWall from "./components/LogoWall/LogoWall.tsx";
 const items = [
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/chatgpt.svg",
+    img: "/icons/chatgpt.svg",
     title: "ChatGPT",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/vite.svg",
+    img: "/icons/vite.svg",
     title: "Vite",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/js.svg",
+    img: "/icons/js.svg",
     title: "Java Script",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/typescript.svg",
+    img: "/icons/typescript.svg",
     title: "Type Script",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/html.svg",
+    img: "/icons/html.svg",
     title: "HTML5",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/css.svg",
+    img: "/icons/css.svg",
     title: "CSS3",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/figma.svg",
+    img: "/icons/figma.svg",
     title: "Figma",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/vsc.svg",
+    img: "/icons/vsc.svg",
     title: "VSCode",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/git.svg",
+    img: "/icons/git.svg",
     title: "Git",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/github.svg",
+    img: "/icons/github.svg",
     title: "GitHub",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/mongodb.svg",
+    img: "/icons/mongodb.svg",
     title: "MongoDB",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/node-js.svg",
+    img: "/icons/node-js.svg",
     title: "Node.JS",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/npm.svg",
+    img: "/icons/npm.svg",
     title: "NPM",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/postman.svg",
+    img: "/icons/postman.svg",
     title: "Postman",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/react-router.svg",
+    img: "/icons/react-router.svg",
     title: "React Router",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/redux.svg",
+    img: "/icons/redux.svg",
     title: "Redux",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/react.svg",
+    img: "/icons/react.svg",
     title: "React.JS",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/sass.svg",
+    img: "/icons/sass.svg",
     title: "Sass",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/tailwind.svg",
+    img: "/icons/tailwind.svg",
     title: "Tailwind",
   },
   {
-    img: "https://github.com/Igor-Tymchuk/tymchuk-portfolio/blob/main/src/assets/icons/vercel.svg",
+    img: "/icons/vercel.svg",
     title: "Vercel",
   },
 ];
